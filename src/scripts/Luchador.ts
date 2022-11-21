bb01d084554ba17243fd562977235db08613e734
mariabragado <mjbragado@gmail.com>
2022-11-21 21:03:00 +0100

    Fixing code smell

--- src/scripts/Luchador.ts
+++ src/scripts/Luchador.ts
@@ -11,9 +11,9 @@ export class Luchador extends Personaje {
         destreza: number
     ) {
         super(name, family, age);
-        (this.arma = arma),
-            (this.destreza = destreza),
-            (this.message = 'Primero pego y luego pregunto');
+        this.arma = arma;
+        this.destreza = destreza;
+        this.message = 'Primero pego y luego pregunto';
     }
     communication(): string {
         return this.message;
